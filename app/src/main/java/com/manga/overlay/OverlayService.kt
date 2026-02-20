ec9e212bb1a554f3240b57b3afacccefb4fbcf57
lynn224 <147479403+lynn224@users.noreply.github.com>
2026-02-20 13:50:14 +0800

    Add files via upload

---
+++ app/src/main/java/com/manga/overlay/OverlayService.kt
@@ -0,0 +1,10 @@
+package com.manga.overlay
+
+import android.app.Activity
+import android.os.Bundle
+
+class OverlayService : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
